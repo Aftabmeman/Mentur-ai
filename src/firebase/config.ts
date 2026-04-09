4e0c1bea63b6fb174b08288be436891c13dd34d0
Learning App development <appdevelopmentlearning@gmail.com>
2026-04-09 13:42:55 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8515730718-27b1e",
+  "appId": "1:417674426575:web:9c1cda1c088fd719679fba",
+  "apiKey": "AIzaSyDLhwHrCrYI1RmthpMyAfyecX80EPwI9Uo",
+  "authDomain": "studio-8515730718-27b1e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "417674426575"
+};
